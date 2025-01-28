954e5af5b61e0232b72183df73e6337d5161b7bc
Peter Reriani <peterkendyr@gmail.com>
2025-01-28 13:57:38 +0300

    Add private and public keys to .gitignore

--- main.tf
+++ main.tf
@@ -1,7 +1,41 @@
-resource "aws_instance" "web_server" {
-  ami           = "ami-0ac4dfaf1c5c0cce9" 
-  instance_type = "t2.micro"
+
+
+resource "tls_private_key" "cloud_base" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+
+
+resource "local_file" "private_key_pem" {
+  filename = "${path.module}/my-key-pair.pem"  
+  content  = tls_private_key.cloud_base.private_key_pem
+}
+
+
+
+resource "local_file" "public_key" {
+  filename = "${path.module}/my-key-pair.pub"
+  content  = tls_private_key.cloud_base.public_key_openssh
+}
+
+
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "cloud_base"
+  public_key = tls_private_key.cloud_base.public_key_openssh
+}
+
+
+resource "aws_instance" "Cloud-Base" {
+  ami           = "ami-0ac4dfaf1c5c0cce9"  
+  instance_type = "t2.micro"  
+  key_name      = aws_key_pair.key_pair.key_name  
+
   tags = {
-    Name = "CloudBase-EC2"
+    Name = "Cloud-Base"
   }
 }
+
+
+
