522455e69cabd6ce79aae1f3d39c521f9cd21e3c
Peter Reriani <peterkendyr@gmail.com>
2025-01-28 11:41:22 +0300

    Added providers.tf

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  shared_config_files      = ["~/.aws/configuration"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "Tuser"
+  region                   = "us-east-1"
+}
